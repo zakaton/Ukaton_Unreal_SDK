987a19ce90e34e21c0ebaab4d30d60c32900b3b0
Zack <zack@ukaton.com>
2023-09-12 09:18:25 -0700

    arrays

--- Source/Ukaton_Unreal_SDK/Public/UkatonPressureValues.h
+++ Source/Ukaton_Unreal_SDK/Public/UkatonPressureValues.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "UkatonPressureValue.h"
+#include "Math/Vector2D.h"
 #include "UkatonPressureValues.generated.h"
 
 USTRUCT(BlueprintType)
@@ -12,6 +13,7 @@ struct FUkatonPressureValues
 	GENERATED_BODY()
 
 	static const uint8 NumberOfPressureSensors = 16;
+	static const FVector2D PressurePositions[NumberOfPressureSensors];
 
 	UPROPERTY(BlueprintReadOnly, Category = "Ukaton")
 	TArray<FUkatonPressureValue> PressureValues;
