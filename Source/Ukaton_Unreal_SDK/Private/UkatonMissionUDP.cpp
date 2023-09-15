4cf679fb9a96b44a8b6cc640ca5b53d540c9404e
Zack <zack@ukaton.com>
2023-09-15 12:06:35 -0700

    Update UkatonMissionUDP.cpp

--- Source/Ukaton_Unreal_SDK/Private/UkatonMissionUDP.cpp
+++ Source/Ukaton_Unreal_SDK/Private/UkatonMissionUDP.cpp
@@ -1,13 +1,31 @@
 // Copyright (c) 2023 Zack Qattan
 
 #include "UkatonMissionUDP.h"
+#include "Logging/StructuredLog.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(UkatonMissionUDP, Log, All);
 
 void AUkatonMissionUDP::Connect(const FString &IPAddress)
 {
+    if (bIsConnecting)
+    {
+        Disconnect();
+    }
+
+    bIsConnecting = true;
+    // FILL
 }
 void AUkatonMissionUDP::Disconnect()
 {
     // FILL
+    if (bIsConnecting)
+    {
+        bIsConnecting = false;
+    }
+    else if (bIsConnected)
+    {
+        bIsConnected = false;
+    }
 }
 
 FString AUkatonMissionUDP::GetAutoConnectDeviceIdentifier() const
