1cf10362d60a6b894a837baaf9f9033f0b55dd87
Zack <zack@ukaton.com>
2023-09-22 08:05:51 -0700

    changes

--- Source/Ukaton_Unreal_SDK/Public/UkatonSensorDataConfigurationsManager.h
+++ Source/Ukaton_Unreal_SDK/Public/UkatonSensorDataConfigurationsManager.h
@@ -10,7 +10,7 @@
 #include "UkatonSensorDataRate.h"
 #include "UkatonSensorDataConfigurationsManager.generated.h"
 
-DECLARE_LOG_CATEGORY_EXTERN(LogUkatonSensorDataConfigurationsManager, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogUkatonSensorDataConfigurationsManager, Log, Warning);
 
 USTRUCT(BlueprintType)
 struct FUkatonSensorDataConfigurationsManager
